5f38c1e47fe80d53168f3e96661ba5ffac37b0eb
Daniel Henderson <tindrum@mac.com>
2023-06-25 13:00:55 -0700

    conveyorBelt canged back to verbose object identification

--- Assets/Scripts/ConveyorBelt.cs
+++ Assets/Scripts/ConveyorBelt.cs
@@ -41,18 +41,21 @@ public class ConveyorBelt : MonoBehaviour
         GameObject theBox = collision.gameObject;
 
         // call the method on the instantiated cardboard box object
-        if (theBox.GetComponent("BoxCenterOfMass") != null)
+        if (collision.gameObject.GetComponent("BoxCenterOfMass") != null)
         {
             // failing the LowerCenterOfMass() call, 
             // perhaps the method exited as a failure 
             // and therefore didn't add other types of objects to the conveyor.
-            theBox.GetComponent<BoxCenterOfMass>().LowerCenterOfMass();
+            collision.gameObject.GetComponent<BoxCenterOfMass>().LowerCenterOfMass();
         }
         //collision.gameObject.GetComponent<BoxCenterOfMass>().LowerCenterOfMass();
         // this test works syntactically: !onBelt.Contains(collision.gameObject
-        if (theBox.CompareTag("Boxes") && !onBelt.Contains(theBox))
+
+        // sort of only want to add boxes to the conveyor, but why?
+        // theBox.CompareTag("Boxes") && 
+        if (!onBelt.Contains(collision.gameObject))
         {
-            onBelt.Add(theBox);
+            onBelt.Add(collision.gameObject);
         }
       }
 
@@ -62,7 +65,7 @@ public class ConveyorBelt : MonoBehaviour
         GameObject theBox = collision.gameObject;
 
         // call the method on the instantiated cardboard box object
-        theBox.GetComponent<BoxCenterOfMass>().ResetCenterOfMass();
-        onBelt.Remove(theBox);
+        collision.gameObject.GetComponent<BoxCenterOfMass>().ResetCenterOfMass();
+        onBelt.Remove(collision.gameObject);
     }
 }
