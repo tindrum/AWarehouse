f33078d968e0ad1ba990c93aa411f741935cb66f
Daniel Henderson <tindrum@mac.com>
2023-06-27 17:43:08 -0700

    I can see the error of my ways

--- Assets/Scripts/PackageDirectorMechanical.cs
+++ Assets/Scripts/PackageDirectorMechanical.cs
@@ -36,15 +36,20 @@ public class PackageDirectorMechanical : MonoBehaviour
     // text debug
     public TMP_Text diagnosticOne;
     public TMP_Text diagnosticTwo;
+    public TMP_Text diagnosticThree;
+    public TMP_Text diagnosticFour;
+    public TMP_Text diagnosticFive;
+    public TMP_Text diagnosticNearAngle;
+    public TMP_Text diagnosticNearCommandedAngle;
 
-    
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-        nearPaddle = transform.Find("NearPaddlePivot");
-        farPaddle = transform.Find("FarPaddlePivot");
+        //nearPaddle = transform.Find("NearPaddlePivot");
+        //farPaddle = transform.Find("FarPaddlePivot");
 
         nearCommandedAngle = 0.0f;
         farCommandedAngle = 0.0f;
@@ -52,33 +57,44 @@ public class PackageDirectorMechanical : MonoBehaviour
         nearEnoughAngleDifference = 3.0f; 
         sweepSpeed = 0.5f; // maximum angular speed
 
-
+        diagnosticOne.text = "Start()";
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        diagnosticOne.text = "Update()";
+        diagnosticNearAngle.text = nearPaddle.transform.eulerAngles.y.ToString();
+        diagnosticNearCommandedAngle.text = nearCommandedAngle.ToString();
+
         if (!SweepAngleAchieved(nearPaddle, nearCommandedAngle))
         {
-            diagnosticTwo.text = "m";
+            diagnosticOne.text = "Update() !SAA true";
 
             RotatePaddle(nearPaddle);
         }
         else
         {
-            diagnosticTwo.text = "0";
+            diagnosticOne.text = "Update() !SAA false";
         }
- 
 
     }
 
     public void RotatePaddle(Transform paddleObject)
     {
+        diagnosticFive.text = "RotatePaddle()";
+
         if (paddleObject != null && paddleObject == nearPaddle)
         {
-            if ((Mathf.Abs(nearCommandedAngle) - Mathf.Abs(paddleObject.transform.eulerAngles.y)) > 0.0f)
+            diagnosticThree.text = "RP notNull & nearPaddle";
+            float angleDifference = (Mathf.Abs(nearCommandedAngle) - Mathf.Abs(paddleObject.transform.eulerAngles.y));
+            diagnosticFour.text = angleDifference.ToString();
+            if (angleDifference > 0.0f)
             {
+                diagnosticThree.text = "RP rotate";
+                diagnosticTwo.text = nearCommandedAngle.ToString();
+
                 nearPaddle.rotation = Quaternion.Euler(new Vector3(0.0f, nearCommandedAngle * Time.deltaTime, 0.0f));
             }
         }
@@ -89,6 +105,7 @@ public class PackageDirectorMechanical : MonoBehaviour
 
     public bool SweepAngleAchieved(Transform paddleObject, float desiredAngle)
     {
+        diagnosticFive.text = "SweepAngleAchieved()";
         if (paddleObject == nearPaddle)
         {
             if (Mathf.Abs(paddleObject.transform.eulerAngles.y - desiredAngle) <= nearEnoughAngleDifference)
@@ -114,6 +131,7 @@ public class PackageDirectorMechanical : MonoBehaviour
     public void CommandSweepAngle(float angle)
     {
 
+        // diagnosticThree.text = angle.ToString();
         speaker.PlayOneShot(leverSwipeUpSound, 0.5f);
         nearCommandedAngle = angle;
         farCommandedAngle = angle;
